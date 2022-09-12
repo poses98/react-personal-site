884d4c8eeea9efabe29b0ab60b37de3638e140eb
virtdcode <pablo98oses@gmail.com>
2022-09-12 13:43:05 +0200

    Added getmenus api call

---
+++ client/src/api/menu.js
@@ -0,0 +1,23 @@
+import { BASE_PATH, API_VERSION } from './config';
+
+export async function getMenusApi(token) {
+  const url = `${BASE_PATH}/${API_VERSION}/get-menus`;
+  const params = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: token,
+    },
+  };
+
+  return fetch(url, params)
+    .then((response) => {
+      return response.json;
+    })
+    .then((result) => {
+      return result;
+    })
+    .catch((err) => {
+      return err.message;
+    });
+}
