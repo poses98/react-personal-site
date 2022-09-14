ca7471f068f789503c79c8f108397bcf758f9cba
virtdcode <pablo98oses@gmail.com>
2022-09-14 16:59:44 +0200

    Added MenuTop and Footer

--- client/src/layouts/LayoutBasic.js
+++ client/src/layouts/LayoutBasic.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import { Layout } from 'antd';
+import { Layout, Row, Col } from 'antd';
+import MenuTop from '../components/Web/MenuTop/MenuTop';
+
 export default function LayoutAdmin(props) {
   const { children } = props;
   const { Header, Content, Footer } = Layout;
+
   return (
+    <Row>
+      <Col lg={4}></Col>
+      <Col lg={16}>
+        <MenuTop />
+        {children}
+        <p>Footer</p>
+      </Col>
+      <Col lg={4}></Col>
+    </Row>
+  );
+
+  /* return (
     <Layout>
       <Layout>
         <Content>{children}</Content>
         <Footer>Pablo Osés</Footer>
       </Layout>
     </Layout>
-  );
+  ); */
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
-export default function Home(){
-    return(
-        <div>
-            <h2>Home</h2>
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <h2>Homepage</h2>
+    </div>
+  );
 }
